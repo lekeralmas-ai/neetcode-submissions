8253053305d8c238f4fcd3589576a8291ee6f7b4
lekeralmas-ai <lekeralmas@gmail.com>
2026-07-03 11:37:55 +0800

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        mock = strs[0]
+        ll = list()
+        for st in strs:
+            curr_l = 0
+            while curr_l < len(st) and curr_l < len(mock) and st[curr_l] == mock[curr_l]:
+                curr_l += 1
+            ll.append(curr_l)
+        return mock[:min(ll)]
+            
