9ffa23a954bec748d0689716e277459192e11375
lekeralmas-ai <lekeralmas@gmail.com>
2026-07-13 13:30:21 +0800

    Add: average-waiting-time - submission-1

---
+++ Data Structures & Algorithms/average-waiting-time/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        time = customers[0][1]
+        waiting_time = list()
+        waiting_time.append(customers[0][1])
+        for i in range(1,len(customers)):
+            if customers[i][0] < customers[i-1][0] + waiting_time[i-1]:
+                new_time = customers[i-1][0] + waiting_time[i-1]-customers[i][0]+customers[i][1]
+                waiting_time.append(new_time)
+                time += new_time
+            else:
+                waiting_time.append(customers[i][1])
+                time += customers[i][1]
+        return time/len(customers)
+
